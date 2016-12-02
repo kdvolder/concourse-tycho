25dbc892db8ef30939de9f7a1e28c3f92bc49414
nsingh <nsingh@pivotal.io>
2016-12-01 16:34:48 -0800

    test project

---
+++ testproject/src/org/eclipse/cft/server/tests/AllTests.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2015 Pivotal Software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * and the Apache License v2.0 is available at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ *  Contributors:
+ *     Pivotal Software, Inc. - initial API and implementation
+ ********************************************************************************/
+package org.eclipse.cft.server.tests;
+
+import org.eclipse.cft.server.tests.core.CloudUtilTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Steffen Pingel
+ */
+public class AllTests {
+
+	public static final String PLUGIN_ID = "org.eclipse.cft.server.tests";
+
+	public static Test suite() {
+		return suite(false);
+	}
+
+	public static Test suite(boolean heartbeat) {
+		TestSuite suite = new ManagedTestSuite(AllTests.class.getName());
+
+		suite.addTestSuite(CloudUtilTest.class);
+
+		return suite;
+	}
+
+}

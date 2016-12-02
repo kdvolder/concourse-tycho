77335be8003407a9433399a6d8890251f3f928cb
Kris De Volder <kdevolder@pivotal.io>
2016-12-01 18:18:49 -0800

    Use xvfb-run to manage Xvfb process lifetime

--- concourse/tasks/mvn-run.sh
+++ concourse/tasks/mvn-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
-Xvfb :99 &
-export DISPLAY=:99
-echo "Waiting for xvfb"
-sleep 10s
-#xhost +
-icewm &
-cd testproject
-echo "Starting maven build"
-mvn clean integration-test
+xvfb-run mvn clean integration-test
+
+# Xvfb :99 &
+# export DISPLAY=:99
+# echo "Waiting for xvfb"
+# sleep 10s
+# #xhost +
+# #icewm &
+# cd testproject
+# echo "Starting maven build"
+# mvn clean integration-test
